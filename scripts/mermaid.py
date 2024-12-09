7dd476fd6a94ad27f6185709cd158a720cfd3f22
Michael Mattig <michael.m@ttig.de>
2024-12-09 11:25:04 +0100

    no more bs4

--- scripts/mermaid.py
+++ scripts/mermaid.py
@@ -1,22 +1,44 @@
-from bs4 import BeautifulSoup
+from html.parser import HTMLParser
 
-# Load the HTML content
-with open('index.html', 'r') as file:
-    soup = BeautifulSoup(file, 'html.parser')
 
-# Create the new script tag
-new_script = soup.new_tag('script', type='module')
-new_script.string = '''
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+        self.in_body = False
+
+    def handle_starttag(self, tag, attrs):
+        self.result.append(self.get_starttag_text())
+        if tag == 'body':
+            self.in_body = True
+
+    def handle_endtag(self, tag):
+        if tag == 'body':
+            self.in_body = False
+            # Add the script tag before closing body tag
+            self.result.append('''
+<script type="module">
 import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs";
 for (const element of document.getElementsByClassName("language-mermaid")) {
     element.classList.add("mermaid");
 }
 mermaid.initialize({ startOnLoad: true });
-'''
+</script>
+''')
+        self.result.append(f'</{tag}>')
 
-# Append the script tag to the end of the body
-soup.body.append(new_script)
+    def handle_data(self, data):
+        self.result.append(data)
+
+
+# Load the HTML content
+with open('./_site/index.html', 'r') as file:
+    content = file.read()
+
+# Parse the HTML content
+parser = MyHTMLParser()
+parser.feed(content)
 
 # Save the modified HTML back to the file
-with open('index.html', 'w') as file:
-    file.write(str(soup))
+with open('./_site/index.html', 'w') as file:
+    file.write(''.join(parser.result))
